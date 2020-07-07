4e41a542b255a6d25317af6ab954029af2ae1af9
Howard Chen <howardplus@gmail.com>
2020-07-07 14:43:23 -0700

    fix bpf program build script

--- example1/bpf_program/build.sh
+++ example1/bpf_program/build.sh
@@ -1,2 +1,3 @@
 # build the bpf program
-clang -O2 -emit-llvm -c bpf_prog.c -o - | llc -march=bpf -filetype=obj -o bpf_prog.o
+KERN_SRC=/usr/src/linux-source-5.4.0/linux-source-5.4.0
+clang -O2 -emit-llvm -I${KERN_SRC}/tools/testing/selftests/bpf -c bpf_prog.c -o - | llc -march=bpf -filetype=obj -o bpf_prog.o
